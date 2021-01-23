ebef593c531a38677b38fc3d4e390035c9589969
Shashwatw7 <75832595+Shashwatw7@users.noreply.github.com>
2021-01-23 15:55:17 +0530

    Add files via upload

---
+++ multiplier.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int i,j;
+ 
+	cout << "Enter the values to be multiplied : "; 
+    cin >> i >> j ;
+    cout<<"The value after multiplication is "<<i*j;
+	return 0; 
+} 
